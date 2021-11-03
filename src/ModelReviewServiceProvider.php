23324e81fdcb87f0d84fb35504554dd36d4eb6bf
Hussein El-Hewehii <husseinhewehii@gmail.com>
2021-11-03 12:01:11 +0200

    package start

---
+++ src/ModelReviewServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+namespace Hewehi\ModelReview;
+
+use Illuminate\Support\ServiceProvider;
+
+class ModelReviewServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        
+    }
+
+    public function boot()
+    {
+        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
+        $this->loadViewsFrom(__DIR__.'/views', 'model-review');
+        $this->publishes([
+            __DIR__ .'/migrations/create_review_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_review_table.php'),
+        ], 'migrations');
+
+        $this->publishes([
+            __DIR__ . '/config/model-review.php' => config_path('model-review.php')
+        ], 'config');
+    }
+}
